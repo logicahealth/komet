748c13012815e6d6d28a4f6c5f2dcdad619e1646
Aimee Furber <furber@informatics.com>
2013-11-27 15:53:22 -0800

    added code review notes, made corrections to preferred term display

--- tcc-dto-ddo/src/main/java/org/ihtsdo/otf/tcc/ddo/concept/component/description/SimpleDescriptionVersionDdo.java
+++ tcc-dto-ddo/src/main/java/org/ihtsdo/otf/tcc/ddo/concept/component/description/SimpleDescriptionVersionDdo.java
@@ -44,6 +44,10 @@ public class SimpleDescriptionVersionDdo extends SimpleVersionDdo {
         super();
     }
 
+/*AKF: Is there way to get only certain parts of a DDO back? 
+I'm not sure how many times we will see a use case like this, 
+but it seems better to not have make new DDO classes each time.
+*/
     public SimpleDescriptionVersionDdo(DescriptionChronicleDdo chronicle, TerminologySnapshotDI ss,
             DescriptionVersionBI dv, ConceptVersionBI cv) throws IOException, ContradictionException {
         super(ss, dv);
@@ -51,16 +55,17 @@ public class SimpleDescriptionVersionDdo extends SimpleVersionDdo {
         this.languageProperty.set(dv.getLang());
         this.typeNid = dv.getTypeNid();
         this.dv = dv;
-        if (cv.getPreferredDescription().equals(dv)) {
+//AKF: changed from cv.getPreferredDescription().equals(dv) to account for more than one preferred term (e.g. US and GB)        
+        if (cv.getDescriptionsPreferredActive().contains(dv)) {
             Collection<? extends RefexVersionBI<?>> usRefexMembers = dv.getRefexMembersActive(cv.getViewCoordinate(), Snomed.US_LANGUAGE_REFEX.getNid());
-            if (!usRefexMembers.isEmpty()) {
+            Collection<? extends RefexVersionBI<?>> gbRefexMembers = dv.getRefexMembersActive(cv.getViewCoordinate(), Snomed.GB_LANGUAGE_REFEX.getNid());
+//AKF: forgot that most of the time the PT is for both US and GB
+            if (!usRefexMembers.isEmpty() && gbRefexMembers.isEmpty()) {
                 this.typeProperty.set("Synonym pt:EN-US");
-            } else {
-
-                Collection<? extends RefexVersionBI<?>> gbRefexMembers = dv.getRefexMembersActive(cv.getViewCoordinate(), Snomed.GB_LANGUAGE_REFEX.getNid());
-                if (!gbRefexMembers.isEmpty()) {
+            } else if (!gbRefexMembers.isEmpty() && usRefexMembers.isEmpty()) {
                     this.typeProperty.set("Synonym pt:EN-GB");
-                }
+            }else{
+                this.typeProperty.set("Synonym pt:EN-US,EN-GB");
             }
         } else if (this.typeNid == Snomed.FULLY_SPECIFIED_DESCRIPTION_TYPE.getNid()) {
             this.typeProperty.set("Fully specified name");
@@ -85,6 +90,7 @@ public class SimpleDescriptionVersionDdo extends SimpleVersionDdo {
         return languageProperty.get();
     }
 
+//AKF: I would say "effectiveTime" to keep with the standard naming
     @XmlElement(name = "dateCreated")
     public String getTimeProperty() {
         return timeProperty.get();
