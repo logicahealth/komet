61800c63705ee056b8020beb2f69382dc9d4a849
Keith E. Campbell <campbell@informatics.com>
2014-08-25 19:01:22 -0700

    Modified Ts to use the HK2Looker rather than dependency injection.

--- tcc-api/src/main/java/org/ihtsdo/otf/tcc/api/store/Ts.java
+++ tcc-api/src/main/java/org/ihtsdo/otf/tcc/api/store/Ts.java
@@ -1,9 +1,6 @@
 package org.ihtsdo.otf.tcc.api.store;
 
-import java.lang.reflect.Method;
 import org.ihtsdo.otf.tcc.lookup.Hk2Looker;
-
-import javax.inject.Inject;
 
 /**
  * Ts is short for Terminology store...
@@ -14,28 +11,14 @@ import javax.inject.Inject;
 
 public class Ts {
 
-    private static Ts singleton;
 
-    @Inject private TerminologyStoreDI store;
-
-    private Ts() {};
-
-   //~--- methods -------------------------------------------------------------
-
-   public static void close() throws Exception {
-      get().shutdown();
-   }
-
+    private static TerminologyStoreDI store;
    //~--- get methods ---------------------------------------------------------
 
    public static TerminologyStoreDI get() {
-      if (singleton == null) {
-          singleton = new Ts();
+      if (store == null) {
+          store = Hk2Looker.get().getService(TerminologyStoreDI.class);
       }
-      return singleton.store;
-   }
-
-   public static TerminologySnapshotDI getGlobalSnapshot() {
-       return get().getGlobalSnapshot();
+      return store;
    }
 }
