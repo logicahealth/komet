d62af6fb9e0f49ddd67e64129b0bc3a52566f181
Aimee Furber <furber@informatics.com>
2014-09-27 14:44:16 -0700

    CE-111 removing debug statements

--- tcc-model-impl/src/main/java/org/ihtsdo/otf/tcc/model/cc/concept/ConceptVersion.java
+++ tcc-model-impl/src/main/java/org/ihtsdo/otf/tcc/model/cc/concept/ConceptVersion.java
@@ -367,10 +367,6 @@ public class ConceptVersion implements ConceptVersionBI, Comparable<ConceptVersi
 
    @Override
    public ConceptAttributeVersionBI getConceptAttributesActive() throws IOException, ContradictionException {
-       //TODO-AKF: just for debugging
-       if(concept.getConceptAttributes() == null){
-           System.out.println("### DEBUG: concept attributes null for concept: " + concept);
-       }
       return concept.getConceptAttributes().getVersion(vc);
    }
 
