91cc4a386c21ee534f577e3623b8b8f8a54a54d5
darmbrust <daniel.armbrust.list@gmail.com>
2015-04-21 00:36:58 -0500

    move constants file into ochre-api, integrate HK2 tooling from isaac-gui, add an expermental SystemStatusService for use by isaac-gui.

--- ochre-api/src/main/java/gov/vha/isaac/ochre/api/LookupService.java
+++ ochre-api/src/main/java/gov/vha/isaac/ochre/api/LookupService.java
@@ -15,66 +15,163 @@
  */
 package gov.vha.isaac.ochre.api;
 
+import gov.va.oia.HK2Utilities.HK2RuntimeInitializer;
+import gov.vha.isaac.ochre.api.constants.Constants;
+import gov.vha.isaac.ochre.util.HeadlessToolkit;
 import java.awt.GraphicsEnvironment;
-import com.sun.javafx.application.PlatformImpl;
 import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.BiConsumer;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.runlevel.RunLevelController;
-import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
 import com.sun.javafx.application.PlatformImpl;
-import gov.vha.isaac.ochre.util.HeadlessToolkit;
 
 /**
  *
  * @author kec
+ * @author <a href="mailto:daniel.armbrust.list@gmail.com">Dan Armbrust</a>
  */
+@SuppressWarnings("restriction")
 public class LookupService {
+    private static final Logger log = LogManager.getLogger();
     private static volatile ServiceLocator looker = null;
     public static final int ISAAC_STARTED_RUNLEVEL = 4;
     public static final int ISAAC_STOPPED_RUNLEVEL = -1;
     private static final Object lock = new Object();
-    
-    public static ServiceLocator get() {
-        if (looker == null)
-        {
-            synchronized (lock)
-            {
-                if (looker == null)
-                {
 
-                    if (GraphicsEnvironment.isHeadless())
-                    {
+    /**
+     * @return the {@link ServiceLocator} that is managing this ISAAC instance
+     */
+    public static ServiceLocator get() {
+        if (looker == null) {
+            synchronized (lock) {
+                if (looker == null) {
+                    if (GraphicsEnvironment.isHeadless()) {
+                        log.info("Installing headless toolkit");
                         HeadlessToolkit.installToolkit();
                     }
-//                    if (GraphicsEnvironment.isHeadless()) {
-//                        System.setProperty("javafx.toolkit", "gov.vha.isaac.ochre.api.HeadlessToolkit");
-//                    }
-//                    String toolkitClass = System.getProperty("javafx.toolkit");
-//                    if (toolkitClass != null && toolkitClass.equals("gov.vha.isaac.ochre.api.HeadlessToolkit")) {
-//                        HeadlessToolkit.setupToolkit();
-//                    }
+
                     PlatformImpl.startup(() -> {
                         // No need to do anything here
                         });
-                    looker = ServiceLocatorUtilities.createAndPopulateServiceLocator();
+                    
+                    ArrayList<String> packagesToSearch = new ArrayList<String>(Arrays.asList("gov.vha", "org.ihtsdo", "org.glassfish"));
+
+                    boolean readInhabitantFiles = Boolean.getBoolean(System.getProperty(Constants.READ_INHABITANT_FILES, "false"));
+                    if (System.getProperty(Constants.EXTRA_PACKAGES_TO_SEARCH) != null) {
+                        String[] extraPackagesToSearch = System.getProperty(Constants.EXTRA_PACKAGES_TO_SEARCH).split(";");
+                        for (String packageToSearch: extraPackagesToSearch) {
+                            packagesToSearch.add(packageToSearch);
+                        }
+                    }
+                    try {
+                        String[] packages = packagesToSearch.toArray(new String[]{});
+                        log.info("Looking for HK2 annotations " + (readInhabitantFiles ? "from inhabitant files" : "skipping inhabitant files") 
+                                + "; and scanning in the packages: " + Arrays.toString(packages));
+                        looker = HK2RuntimeInitializer.init("ISAAC", readInhabitantFiles, packages);
+                        log.info("HK2 initialized.  Identifed " + looker.getAllServiceHandles((criteria) -> {return true;}).size() + " services");
+                    }
+                    catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
         }
         return looker;
     }
 
+    /**
+     * Return the highest ranked service that implements the requested contract or implementation class.
+     * @see ServiceLocator#getService(Class, Annotation...) 
+     */
     public static <T> T getService(Class<T> contractOrImpl) {
         return get().getService(contractOrImpl, new Annotation[0]);
+    }
+    
+    /**
+     * Find the best ranked service with the specified name.  If no service with the specified name is available, 
+     * this returns null (even if there is a service with another name [or no name] which would meet the contract)
+     * 
+     * @see ServiceLocator#getService(Class, String, Annotation...)
+     * 
+     * @param contractOrService May not be null, and is the contract or concrete implementation to get the best instance of
+     * @param name May not be null or empty
+     */
+    public static <T> T getService(Class<T> contractOrService, String name) {
+        if (StringUtils.isEmpty(name)) {
+            throw new IllegalArgumentException("You must specify a service name to use this method");
+        }
+        return get().getService(contractOrService, name, new Annotation[0]);
+    }
+    
+    /**
+     * Find a service by name, and automatically fall back to any service which implements the contract if the named service was not available.
+     * 
+     * @param contractOrService May not be null, and is the contract or concrete implementation to get the best instance of
+     * @param name May be null (to indicate any name is ok), and is the name of the implementation to be returned
+     */
+    public static <T> T getNamedServiceIfPossible(Class<T> contractOrService, String name) {
+        if (StringUtils.isEmpty(name)) {
+            return get().getService(contractOrService);
+        }
+        else {
+            T service = get().getService(contractOrService, name);
+            if (service == null) {
+                service = get().getService(contractOrService);
+            }
+            return service;
+        }
     }
 
     public static RunLevelController getRunLevelController() {
         return getService(RunLevelController.class);
-    }    
+    }
     
+    /**
+     * Start all core isaac services, blocking until started (or failed)
+     */
     public static void startupIsaac() {
         getRunLevelController().proceedTo(ISAAC_STARTED_RUNLEVEL);
     }
+    
+    /**
+     * Stop all core isaac service, blocking until stopped (or failed)
+     */
     public static void shutdownIsaac() {
-        getRunLevelController().proceedTo(ISAAC_STOPPED_RUNLEVEL);        
+        getRunLevelController().proceedTo(ISAAC_STOPPED_RUNLEVEL);
+    }
+    
+    /**
+     * start all core isaac services in a background thread, returning immediately.  
+     * @param callWhenStartComplete (optional) - if provided,  a call back will be provided
+     * notifying of successfully start of ISAAC, or providing the Exception, if the startup sequence failed.
+     */
+    public static void startupIsaac(BiConsumer<Boolean, Exception> callWhenStartComplete) {
+        log.info("Background starting ISAAC services");
+        Thread backgroundLoad = new Thread(() ->
+        {
+            try {
+                startupIsaac();
+                log.info("Background start complete - runlevel now " + getRunLevelController().getCurrentRunLevel());
+                if (callWhenStartComplete != null) {
+                    callWhenStartComplete.accept(isIssacStarted(), null);
+                }
+            }
+            catch (Exception e) {
+                log.warn("Background start failed - runlevel now " + getRunLevelController().getCurrentRunLevel(), e);
+                if (callWhenStartComplete != null) {
+                    callWhenStartComplete.accept(false, e);
+                }
+            }
+        }, "Datastore init thread");
+        backgroundLoad.start();
+    }
+    
+    public static boolean isIssacStarted() {
+        return getRunLevelController().getCurrentRunLevel() == ISAAC_STARTED_RUNLEVEL;
     }
 }
