cc0247dda093e61360c29c94abaee9ed8c8de172
darmbrust <daniel.armbrust.list@gmail.com>
2015-04-28 19:23:43 -0500

    remove constants that don't exist as concepts

--- tcc-api/src/main/java/org/ihtsdo/otf/tcc/api/metadata/binding/Taxonomies.java
+++ tcc-api/src/main/java/org/ihtsdo/otf/tcc/api/metadata/binding/Taxonomies.java
@@ -5,14 +5,6 @@ import java.util.UUID;
 import org.ihtsdo.otf.tcc.api.spec.ConceptSpec;
 
 public class Taxonomies {
-
-	public static ConceptSpec WB_AUX = 
-		new ConceptSpec("Terminology Auxiliary concept", 
-				UUID.fromString("f4d2fabc-7e96-3b3a-a348-ae867ba74029"));
-	
-	public static ConceptSpec REFSET_AUX = 
-		new ConceptSpec("Refset Auxiliary Concept", 
-				UUID.fromString("1c698388-c309-3dfa-96f0-86248753fac5"));
 	
 	public static ConceptSpec QUEUE_TYPE = 
 		new ConceptSpec("Queue Type", 
