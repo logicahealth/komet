733ed4167d16180dfaf07bab1573a107021fe5f4
darmbrust <daniel.armbrust.list@gmail.com>
2015-06-18 17:42:42 -0500

    taskify the init method, add utility methods to launch the task in a thread.

--- ochre-api/src/main/java/gov/vha/isaac/ochre/api/classifier/ClassifierService.java
+++ ochre-api/src/main/java/gov/vha/isaac/ochre/api/classifier/ClassifierService.java
@@ -15,10 +15,12 @@
  */
 package gov.vha.isaac.ochre.api.classifier;
 
+import gov.vha.isaac.ochre.api.LookupService;
 import gov.vha.isaac.ochre.api.coordinate.EditCoordinate;
 import gov.vha.isaac.ochre.api.coordinate.LogicCoordinate;
 import gov.vha.isaac.ochre.api.coordinate.StampCoordinate;
 import gov.vha.isaac.ochre.collections.ConceptSequenceSet;
+import gov.vha.isaac.ochre.util.WorkExecutors;
 import javafx.concurrent.Task;
 import org.jvnet.hk2.annotations.Contract;
 
@@ -28,13 +30,51 @@ import org.jvnet.hk2.annotations.Contract;
  */
 @Contract
 public interface ClassifierService {
+
+    /**
+     * @param logicCoordinate
+     * @return Return the (unstarted) task which will initialize the classifier
+     */
+    Task<Void> initialize(LogicCoordinate logicCoordinate);
+
+    /**
+     * Starts the {@link #initialize(LogicCoordinate)} in a background thread, return the task handle 
+     * @param logicCoordinate
+     * @return Return the started task which performs initialize
+     */
+    default Task<Void> startInitialize(LogicCoordinate logicCoordinate)
+    {
+        Task<Void> t = initialize(logicCoordinate);
+        LookupService.getService(WorkExecutors.class).getExecutor().execute(t);
+        return t;
+    }
     
-    void initialize(LogicCoordinate logicCoordinate);
-    
+    /**
+     * @param stampCoordinate
+     * @param logicCoordinate
+     * @param editCoordinate
+     * @return Return the (unstarted) task which will classify
+     */
     Task<ClassifierResults> fullClassification(
             StampCoordinate stampCoordinate, 
             LogicCoordinate logicCoordinate, 
             EditCoordinate editCoordinate);
+    
+    /**
+     * Starts the {@link #fullClassification(StampCoordinate, LogicCoordinate, EditCoordinate)} in a background thread, return the task handle 
+     * @param stampCoordinate
+     * @param logicCoordinate
+     * @param editCoordinate
+     * @return Return the started task which performs classify
+     */
+    default Task<ClassifierResults> startFullClassification(
+            StampCoordinate stampCoordinate, 
+            LogicCoordinate logicCoordinate, 
+            EditCoordinate editCoordinate) {
+        Task<ClassifierResults> t = fullClassification(stampCoordinate, logicCoordinate, editCoordinate);
+        LookupService.getService(WorkExecutors.class).getExecutor().execute(t);
+        return t;
+    }
     
     ClassifierResults incrementalClassification(StampCoordinate stampCoordinate, 
             LogicCoordinate logicCoordinate, 
