8bc78ccdc6a54c0dd7aea27dc8ea0ac3bbf52bcc
Joel Kniaz <joel.kniaz@gmail.com>
2015-07-24 07:02:24 -0400

    Removing generic parameterization from CommitServiceMethods

--- ochre-api/src/main/java/gov/vha/isaac/ochre/api/commit/CommitService.java
+++ ochre-api/src/main/java/gov/vha/isaac/ochre/api/commit/CommitService.java
@@ -45,25 +45,25 @@ public interface CommitService {
     
     Optional<String> getComment(int stampSequence);
     
-    Task<Void> addUncommitted(ConceptChronology<? extends ConceptVersion<?>> cc);
+    Task<Void> addUncommitted(ConceptChronology<?> cc);
 
-    Task<Void> addUncommittedNoChecks(ConceptChronology<? extends ConceptVersion<?>> cc);
+    Task<Void> addUncommittedNoChecks(ConceptChronology<?> cc);
 
-    Task<Void> addUncommitted(SememeChronology<? extends SememeVersion<?>> sc);
+    Task<Void> addUncommitted(SememeChronology<?> sc);
 
-    Task<Void> addUncommittedNoChecks(SememeChronology<? extends SememeVersion<?>> sc);
+    Task<Void> addUncommittedNoChecks(SememeChronology<?> sc);
 
     Task<Void> cancel();
 
-    Task<Void> cancel(ConceptChronology<? extends ConceptVersion<?>> chronicledConcept);
+    Task<Void> cancel(ConceptChronology<?> chronicledConcept);
 
-    Task<Void> cancel(SememeChronology<? extends SememeVersion<?>> sememeChronicle);
+    Task<Void> cancel(SememeChronology<?> sememeChronicle);
 
     Task<Optional<CommitRecord>> commit(String commitComment);
 
-    Task<Optional<CommitRecord>> commit(ConceptChronology<? extends ConceptVersion<?>> chronicledConcept, String commitComment);
+    Task<Optional<CommitRecord>> commit(ConceptChronology<?> chronicledConcept, String commitComment);
     
-    Task<Optional<CommitRecord>> commit(SememeChronology<? extends SememeVersion<?>> sememeChronicle, String commitComment);
+    Task<Optional<CommitRecord>> commit(SememeChronology<?> sememeChronicle, String commitComment);
     
     ObservableList<Integer> getUncommittedConceptNids();
     
