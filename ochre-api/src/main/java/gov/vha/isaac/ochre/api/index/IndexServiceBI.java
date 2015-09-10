3432c49b591e96571c2b1dc32f3eea645d228f90
darmbrust <daniel.armbrust.list@gmail.com>
2015-09-10 01:37:04 -0500

    add another method to the higher level interface (this was already implemented)

--- ochre-api/src/main/java/gov/vha/isaac/ochre/api/index/IndexServiceBI.java
+++ ochre-api/src/main/java/gov/vha/isaac/ochre/api/index/IndexServiceBI.java
@@ -124,8 +124,7 @@ public interface IndexServiceBI {
      * @param query The query to apply.
      * @param sizeLimit The maximum size of the result list.
      * @return a List of {@code SearchResult</codes> that contains the nid of the
-     * component that matched, and the score of that match relative to other
-     * matches.
+     * component that matched, and the score of that match relative to other matches.
      */
     List<SearchResult> query(String query, int sizeLimit);
 
@@ -138,11 +137,38 @@ public interface IndexServiceBI {
      * or the concept sequence {@link IsaacMetadataAuxiliaryBinding#SNOMED_INTEGER_ID} for example.
      * @param sizeLimit The maximum size of the result list.  Pass Integer.MAX_VALUE for unlimited results.
      * @param targetGeneration target generation that must be included in the search
-     * or Long.MIN_VALUE if there is no need to wait for a target generation.
+     * or Long.MIN_VALUE if there is no need to wait for a target generation.  Long.MAX_VALUE can be passed in to force this query to wait until 
+     * any in progress indexing operations are completed - and then use the latest index.
+     * 
      * @return a List of {@code SearchResult</codes> that contains the nid of the
-     * component that matched, and the score of that match relative to other
-     * matches.
+     * component that matched, and the score of that match relative to other matches.
      */
     List<SearchResult> query(String query, Integer sememeConceptSequence, int sizeLimit, long targetGeneration);
-
+    
+    /**
+     * @param query The query to apply.
+     * @param prefixSearch if true, utilize a search algorithm that is optimized for prefix searching, such as the searching 
+     * that would be done to implement a type-ahead style search.  Does not use the Lucene Query parser.  Every term (or token) 
+     * that is part of the query string will be required to be found in the result.
+     * 
+     * Note, it is useful to NOT trim the text of the query before it is sent in - if the last word of the query has a 
+     * space character following it, that word will be required as a complete term.  If the last word of the query does not 
+     * have a space character following it, that word will be required as a prefix match only.
+     * 
+     * For example:
+     * The query "family test" will return results that contain 'Family Testudinidae'
+     * The query "family test " will not match on  'Testudinidae', so that will be excluded.
+     * 
+     * @param semeneConceptSequence optional - The concept seqeuence of the sememe that you wish to search within.  If null, 
+     * searches all indexed content.  This would be set to the concept sequence of {@link IsaacMetadataAuxiliaryBinding#DESCRIPTION_ASSEMBLAGE}
+     * or the concept sequence {@link IsaacMetadataAuxiliaryBinding#SNOMED_INTEGER_ID} for example.
+     * @param sizeLimit The maximum size of the result list.  Pass Integer.MAX_VALUE for unlimited results.
+     * @param targetGeneration target generation that must be included in the search or Long.MIN_VALUE if there is no need 
+     * to wait for a target generation.  Long.MAX_VALUE can be passed in to force this query to wait until any in progress 
+     * indexing operations are completed - and then use the latest index.
+     *
+     * @return a List of {@link SearchResult} that contains the nid of the component that matched, and the score of that match relative 
+     * to other matches.
+     */
+    List<SearchResult> query(String query, boolean prefixSearch, Integer sememeConceptSequence, int sizeLimit, Long targetGeneration);
 }
