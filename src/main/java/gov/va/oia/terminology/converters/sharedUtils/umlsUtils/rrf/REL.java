91a76c0152b617741fb1a9ce634b4b38b17e79ff
darmbrust <daniel.armbrust.list@gmail.com>
2015-09-14 16:47:08 -0500

    simplify code, move more specific code back into RxNorm converter

--- src/main/java/gov/va/oia/terminology/converters/sharedUtils/umlsUtils/rrf/REL.java
+++ src/main/java/gov/va/oia/terminology/converters/sharedUtils/umlsUtils/rrf/REL.java
@@ -1,6 +1,6 @@
 package gov.va.oia.terminology.converters.sharedUtils.umlsUtils.rrf;
 
-import gov.va.oia.terminology.converters.sharedUtils.umlsUtils.RRFBaseConverterMojo;
+import gov.va.oia.terminology.converters.sharedUtils.umlsUtils.Relationship;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -8,23 +8,23 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 
 public class REL
 {
-	private String cui1, aui1, stype1, rel, cui2, aui2, stype2, rela, rui, srui, sab, sl, rg, dir, suppress, cvf, targetSAB, targetCODE, rxNormTargetCUI, sourceSAB;
+	private String cui1, aui1, stype1, rel, cui2, aui2, stype2, rela, rui, srui, sab, sl, rg, dir, suppress, cvf, targetSAB, targetCODE, sourceSAB;
 	
 	private UUID sourceUUID_, targetUUID_, relHash_;
 	
 	private boolean lookedUp2_;
 	
-	private boolean snomedSpecialHandling_ = false;
-	
-	public static List<REL> read(String sourceSab, ResultSet rs, boolean lookedUp2, Set<String> allowedCUIs, AtomicInteger cuiSkipCounter, RRFBaseConverterMojo bc) throws SQLException
+	public static List<REL> read(String sourceSab, ResultSet rs, boolean lookedUp2, Set<String> allowedCUIs, AtomicInteger cuiSkipCounter, 
+			boolean isRxNorm, Function<String, String> relReverser) throws SQLException
 	{
 		ArrayList<REL> result = new ArrayList<>();
 		while (rs.next())
 		{
-			REL rel = new REL(sourceSab, rs, lookedUp2, bc);
+			REL rel = new REL(sourceSab, rs, lookedUp2, isRxNorm, relReverser);
 			if (allowedCUIs != null && (!allowedCUIs.contains(rel.cui1) || !allowedCUIs.contains(rel.cui2)))
 			{
 				cuiSkipCounter.getAndIncrement();
@@ -36,16 +36,16 @@ public class REL
 		return result;
 	}
 	
-	private REL(String sourceSab, ResultSet rs, boolean lookedUp2, RRFBaseConverterMojo bc) throws SQLException
+	private REL(String sourceSab, ResultSet rs, boolean lookedUp2, boolean isRxNorm, Function<String, String> relReverser) throws SQLException
 	{
 		sourceSAB = sourceSab;
 		lookedUp2_ = lookedUp2;
-		cui1 = rs.getString(bc.isRxNorm ? "RXCUI1" : "CUI1");
-		aui1 = rs.getString(bc.isRxNorm ? "RXAUI1" : "AUI1");
+		cui1 = rs.getString(isRxNorm ? "RXCUI1" : "CUI1");
+		aui1 = rs.getString(isRxNorm ? "RXAUI1" : "AUI1");
 		stype1 = rs.getString("STYPE1");
 		rel = rs.getString("REL");
-		cui2 = rs.getString(bc.isRxNorm ? "RXCUI2" : "CUI2");
-		aui2 = rs.getString(bc.isRxNorm ? "RXAUI2" : "AUI2");
+		cui2 = rs.getString(isRxNorm ? "RXCUI2" : "CUI2");
+		aui2 = rs.getString(isRxNorm ? "RXAUI2" : "AUI2");
 		stype2 = rs.getString("STYPE2");
 		rela = rs.getString("RELA");
 		rui = rs.getString("RUI");
@@ -62,16 +62,12 @@ public class REL
 			//when the AUI is not null, we have a couple extra vars to read
 			targetSAB = rs.getString("targetSAB");
 			targetCODE = rs.getString("targetCODE");
-			if (bc.isRxNorm)
-			{
-				rxNormTargetCUI = rs.getString("targetCUI");
-			}
 		}
 		
 		if (!lookedUp2_)
 		{
-			rel = bc.reverseRel(rel);
-			rela = bc.reverseRel(rela);
+			rel = relReverser.apply(rel);
+			rela = relReverser.apply(rela);
 		}
 	}
 
@@ -88,14 +84,6 @@ public class REL
 	public String getTargetCUI()
 	{
 		return lookedUp2_ ? cui1 : cui2;
-	}
-	
-	/**
-	 * Hack for RXNorm - because it doesn't have CUIs in the MRREL table.
-	 */
-	public String getRxNormTargetCUI()
-	{
-		return rxNormTargetCUI;
 	}
 	
 	public String getTargetAUI()
@@ -200,17 +188,6 @@ public class REL
 		return sourceSAB;
 	}
 	
-	public boolean hasSnomedSpecialHandling()
-	{
-		return snomedSpecialHandling_;
-	}
-	
-	public void setSnomedUUIDTarget(UUID target)
-	{
-		setTargetUUID(target);
-		snomedSpecialHandling_ = true;
-	}
-	
 	public UUID getRelHash()
 	{
 		if (relHash_ == null)
@@ -220,28 +197,16 @@ public class REL
 		return relHash_;
 	}
 	
-	public UUID getInverseRelHash(RRFBaseConverterMojo bc)
+	public UUID getInverseRelHash(Function<String, Relationship> nameToRelMapper)
 	{
 		//reverse the direction of the rels, and the source/target
-		String relInverse = bc.nameToRel_.get(rel).getFSNName();
+		String relInverse = nameToRelMapper.apply(rel).getFSNName();
 		String relaInverse = null;
 		if (rela != null)
 		{
-			relaInverse = bc.nameToRel_.get(rela).getFSNName();
+			relaInverse = nameToRelMapper.apply(rela).getFSNName();
 		}
 		
 		return UUID.nameUUIDFromBytes(new String(relInverse + relaInverse + targetUUID_ + sourceUUID_).getBytes()); 
-	}
-	
-	public String getSourceTargetAnnotationLabel()
-	{
-		if (getSourceAUI() == null || getTargetAUI() == null)
-		{
-			return null;
-		}
-		else
-		{
-			return getSourceAUI() + " -> " + getTargetAUI();
-		}
 	}
 }
