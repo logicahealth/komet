1b9b0b3a400e3d9fdb4150794ded237a60ff5d97
Keith E. Campbell <campbell@informatics.com>
2016-01-13 17:35:00 -0800

    Improved documentation of stamped version.

--- ochre-api/src/main/java/gov/vha/isaac/ochre/api/chronicle/StampedVersion.java
+++ ochre-api/src/main/java/gov/vha/isaac/ochre/api/chronicle/StampedVersion.java
@@ -18,20 +18,56 @@ package gov.vha.isaac.ochre.api.chronicle;
 import gov.vha.isaac.ochre.api.State;
 
 /**
- *
+ * The chronicle objects use {@code StampedVersion} objects to
+ * provide a means to generically represent the revisions to a component over time,
+ * and to index those revisions by state (active, inactive),
+ * effective time of change,
+ * author of change, module within which the change occurred (international edition, US extension, etc.),
+ * and the development path of the change (development, release candidate, etc.).
+ * Taken together, these fields can be referred to as a versions STAMP
+ * (state, time, author, module, and path).
+ * The version STAMP provides a foundation for version control and
+ * configuration management of all the components of the information architecture.
  * @author kec
  */
 public interface StampedVersion {
 
+	/**
+	 * A sequence starting at one that uniquely identifies a particular combination of
+	 * state, time, author, module, and path. Stamp sequences are idempotent, and there will be
+	 * only one stamp sequence for any unique combination of values.
+	 * @return
+     */
 	int getStampSequence();
 
+	/**
+	 *
+	 * @return the state of this version.
+     */
 	State getState();
 
+	/**
+	 *
+	 * @return the transaction time of commit ov this version.
+     */
 	long getTime();
 
+	/**
+	 *
+	 * @return the sequence of the concept that represents the author who committed this version.
+     */
 	int getAuthorSequence();
 
+	/**
+	 *
+	 * @return the sequence of the concept that represents the module this version is part of.
+     */
 	int getModuleSequence();
+
+	/**
+	 *
+	 * @return the sequence of the path on which this version is committed.
+     */
 
 	int getPathSequence();
 
