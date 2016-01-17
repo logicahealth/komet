2ef442ce232f013a2e18390166d9bdbfd91d578e
darmbrust <daniel.armbrust.list@gmail.com>
2016-01-17 10:06:06 -0600

    bring back the code that allows execution of mojos that are defined outside of this package - to work around a limitation of maven that you cannot share a classloader across multiple mojos.

---
+++ ochre-mojo/src/main/java/gov/vha/isaac/ochre/mojo/external/ExternalCodeExecutor.java
@@ -0,0 +1,142 @@
+/**
+ * Copyright Notice
+ *
+ * This is a work of the U.S. Government and is not subject to copyright
+ * protection in the United States. Foreign copyrights may apply.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gov.vha.isaac.ochre.mojo.external;
+
+import gov.vha.isaac.ochre.api.LookupService;
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.Iterator;
+import java.util.Map;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * Goal which executes mojo-like things that can't be run as mojos directly, due to class loading issues in maven.
+ * 
+ * Basically, the short of it, is that a DB lifecycle can't span multiple plugins in maven. because of classloader
+ * isolation among the plugins.  This class allows us to run any code, even code not in this plugin module - so 
+ * long as it extends the {@link QuasiMojo} class.
+ * 
+ * @see QuasiMojo
+ */
+@Mojo(defaultPhase = LifecyclePhase.PROCESS_RESOURCES, name = "quasi-mojo-executor")
+public class ExternalCodeExecutor extends AbstractMojo
+{
+	@Parameter(required = true, defaultValue = "${project.version}") 
+	protected String projectVersion;
+
+	@Parameter(required = true, defaultValue = "${project.build.directory}") 
+	protected File outputDirectory;
+
+	@Parameter(required = true) protected 
+	String quasiMojoName;
+
+	@Parameter(required = false) protected 
+	Map<String, String> parameters;
+
+	/**
+	 * @see org.apache.maven.plugin.Mojo#execute()
+	 */
+	@Override
+	public void execute() throws MojoExecutionException
+	{
+		try
+		{
+			getLog().info("Executing " + quasiMojoName);
+			long start = System.currentTimeMillis();
+			QuasiMojo quasiMojo = LookupService.getService(QuasiMojo.class, quasiMojoName);
+			
+			if (quasiMojo == null)
+			{
+				throw new MojoExecutionException("Could not locate a QuasiMojo implementation with the name '" + quasiMojoName + "'.");
+			}
+			
+			quasiMojo.outputDirectory = outputDirectory;
+			quasiMojo.projectVersion = projectVersion;
+			quasiMojo.log_ = getLog();
+			
+			if (parameters != null && parameters.size() > 0)
+			{
+				Class<?> myClass = quasiMojo.getClass();
+				Iterator<String> params = parameters.keySet().iterator();
+				while (params.hasNext())
+				{
+					String name = params.next();
+					String value = parameters.get(name);
+					params.remove();
+
+					try
+					{
+						Field myField = myClass.getDeclaredField(name);
+						myField.setAccessible(true);
+
+						if (myField.getType().equals(String.class))
+						{
+							myField.set(quasiMojo, value);
+						}
+						else if (myField.getType().equals(File.class))
+						{
+							myField.set(quasiMojo, new File(value));
+						}
+						else if (myField.getType().equals(Integer.class))
+						{
+							myField.set(quasiMojo, Integer.parseInt(value));
+						}
+						else if (myField.getType().equals(Long.class))
+						{
+							myField.set(quasiMojo, Long.parseLong(value));
+						}
+						else if (myField.getType().equals(Boolean.class))
+						{
+							myField.set(quasiMojo, Boolean.parseBoolean(value));
+						}
+						else
+						{
+							throw new MojoExecutionException("Can't handle field datatype " + myField.getType());
+						}
+						
+					}
+					catch (NoSuchFieldException e)
+					{
+						throw new MojoExecutionException("No field in " + quasiMojo + " to place the parameter " + name + " : " + value);
+					}
+				}
+				
+				if (parameters.size() > 0)
+				{
+					for (String s : parameters.keySet())
+					{
+						getLog().warn("Mojo specified a parameter '" + s + "' that couldn't be placed into the execution class!");
+					}
+				}
+			}
+			
+			quasiMojo.execute();
+
+			getLog().info(quasiMojoName + " execution completed in " + (System.currentTimeMillis() - start) + "ms");
+		}
+		catch (Exception e)
+		{
+			throw new MojoExecutionException("QuasiMojo Execution Failure", e);
+		}
+	}
+}
---
+++ ochre-mojo/src/main/java/gov/vha/isaac/ochre/mojo/external/QuasiMojo.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright Notice
+ *
+ * This is a work of the U.S. Government and is not subject to copyright
+ * protection in the United States. Foreign copyrights may apply.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gov.vha.isaac.ochre.mojo.external;
+
+import java.io.File;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
+import org.jvnet.hk2.annotations.Contract;
+
+/**
+ * {@link QuasiMojo}
+ * 
+ * Implement this class, and add a '@Service(name = "your-execution-name")' annotation to the code.
+ * 
+ * Your code can then be executed with a configuration like this:
+ * 
+ * 	<plugin>
+ * 		<groupId>gov.vha.isaac.ochre.modules</groupId>
+ * 		<artifactId>ochre-mojo</artifactId>
+ * 		<dependencies>
+ * 			<dependency>
+ * 				<groupId>group.that.contains.your.code</groupId>
+ * 				<artifactId>artifact.that.contains.your.code</artifactId>
+ * 				<version>${your.code.version}</version>
+ * 			</dependency>
+ * 		</dependencies>
+ * 		<executions>
+ * 			<execution>
+ * 			<id>your-id</id>
+ * 			<goals>
+ * 				<goal>quasi-mojo-executor</goal>
+ * 			</goals>
+ * 			<configuration>
+ * 				<quasiMojoName>your-execution-name</quasiMojoName>  <!--must match @Service annotation name -->
+ * 				<parameters>
+ * 					<loincFileLocation>${project.build.directory}/generated-resources/loinc</loincFileLocation>
+ * 					<loincTPFileLocation>${project.build.directory}/generated-resources/loincTP</loincTPFileLocation>
+ * 					<loaderVersion>${loader.version}</loaderVersion>
+ * 				</parameters>
+ * 			</configuration>
+ * 			</execution>
+ * 		</executions>
+ * 	</plugin>
+ * 
+ * Configuration to your class is passed via the <parameters> section.  Each item within the parameters _must_ have a matching 
+ * parameter name in your implementation class.  Basic types (String, File, Integer, Boolean, etc) are handled automatically.
+ * 
+ * This allows your code to be executed within the same classloader which does DB / ISAAC things - (other executions 
+ * within the isaac-mojo package) but lets us not worry about dependency chain issues, as your class is found at runtime
+ * via HK2 injection.
+ * 
+ * @author <a href="mailto:daniel.armbrust.list@gmail.com">Dan Armbrust</a>
+ */
+@Contract
+public abstract class QuasiMojo
+{
+	protected Log log_ = null;
+	protected String projectVersion;
+	protected File outputDirectory;
+	
+	public abstract void execute() throws MojoExecutionException;
+	
+	public Log getLog()
+	{
+		return log_;
+	}
+}
