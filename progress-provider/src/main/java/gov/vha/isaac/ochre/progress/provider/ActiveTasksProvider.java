a5df6f8fbc04972b4764cd2fb48d4f65cd8384db
darmbrust <daniel.armbrust.list@gmail.com>
2016-01-17 10:01:28 -0600

    Fix the task provider, the previous implementation was requiring the Task objects to be Comparable (which they are not)

--- progress-provider/src/main/java/gov/vha/isaac/ochre/progress/provider/ActiveTasksProvider.java
+++ progress-provider/src/main/java/gov/vha/isaac/ochre/progress/provider/ActiveTasksProvider.java
@@ -1,12 +1,11 @@
 package gov.vha.isaac.ochre.progress.provider;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.inject.Singleton;
+import org.jvnet.hk2.annotations.Service;
 import gov.vha.isaac.ochre.api.progress.ActiveTasks;
 import javafx.concurrent.Task;
-import org.jvnet.hk2.annotations.Service;
-
-import javax.inject.Singleton;
-import java.util.Set;
-import java.util.concurrent.ConcurrentSkipListSet;
 
 /**
  * Created by kec on 1/2/16.
@@ -14,7 +13,7 @@ import java.util.concurrent.ConcurrentSkipListSet;
 @Service
 @Singleton
 public class ActiveTasksProvider implements ActiveTasks {
-    ConcurrentSkipListSet<Task<?>> taskSet = new ConcurrentSkipListSet<>();
+    Set<Task<?>> taskSet = ConcurrentHashMap.newKeySet();
 
     @Override
     public Set<Task<?>> get() {
