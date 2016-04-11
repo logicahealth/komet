57f870a4148ac053784bddb58677f53099c9d6dd
darmbrust <daniel.armbrust.list@gmail.com>
2016-04-11 15:36:56 -0400

    add a missing interface for consistency

--- ochre-api/src/main/java/gov/vha/isaac/ochre/api/component/concept/ConceptVersion.java
+++ ochre-api/src/main/java/gov/vha/isaac/ochre/api/component/concept/ConceptVersion.java
@@ -1,12 +1,13 @@
 package gov.vha.isaac.ochre.api.component.concept;
 
+import gov.vha.isaac.ochre.api.commit.CommittableComponent;
 import gov.vha.isaac.ochre.api.identity.StampedVersion;
 
 /**
  * Created by kec on 6/6/15.
  * @param <V>
  */
-public interface ConceptVersion<V extends ConceptVersion<V>> extends StampedVersion {
+public interface ConceptVersion<V extends ConceptVersion<V>> extends StampedVersion, CommittableComponent {
 
     ConceptChronology<V> getChronology();
 
