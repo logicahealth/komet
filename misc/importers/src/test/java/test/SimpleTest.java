896d9e4c31c80c4177af7bbeb363e791719f660b
darmbrust <daniel.armbrust.list@gmail.com>
2017-12-28 17:14:24 -0600

    getting importer code to compile - big merge still not complete

---
+++ misc/importers/src/test/java/test/SimpleTest.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright Notice
+ *
+ * This is a work of the U.S. Government and is not subject to copyright
+ * protection in the United States. Foreign copyrights may apply.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.SystemStreamLog;
+
+import sh.isaac.api.LookupService;
+import sh.isaac.api.bootstrap.TermAux;
+import sh.isaac.api.constants.Constants;
+import sh.isaac.misc.importers.VHATDeltaImport;
+import sh.isaac.mojo.IndexTermstore;
+import sh.isaac.mojo.LoadTermstore;
+
+/**
+ * {@link SimpleTest}
+ *
+ * @author <a href="mailto:daniel.armbrust.list@gmail.com">Dan Armbrust</a>
+ */
+public class SimpleTest
+{
+	public static void main(String[] args) throws MojoExecutionException, IOException
+	{
+		try
+		{
+			File db = new File("target/db");
+			FileUtils.deleteDirectory(db);
+			db.mkdirs();
+			System.setProperty(Constants.DATA_STORE_ROOT_LOCATION_PROPERTY, db.getCanonicalPath());
+			LookupService.startupIsaac();
+			LoadTermstore lt = new LoadTermstore();
+			lt.setLog(new SystemStreamLog());
+			lt.setibdfFilesFolder(new File("../../metadata/target/generated-resource/"));
+			lt.execute();
+			new IndexTermstore().execute();
+			VHATDeltaImport i = new VHATDeltaImport(
+				//new String(Files.readAllBytes(Paths.get("src/test/resources/VHAT XML Update files/Test File 2.xml"))),
+				new String(Files.readAllBytes(Paths.get("src/test/resources/VHAT XML Update files/Test File 1.xml"))),
+				TermAux.USER.getPrimordialUuid(), TermAux.VHAT_EDIT.getPrimordialUuid(), TermAux.DEVELOPMENT_PATH.getPrimordialUuid(),
+				null, new File("target"));
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			LookupService.shutdownSystem();
+			System.exit(0);
+		}
+		
+	}
+}
