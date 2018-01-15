2fed8673a78bf496ca56e2aaf04fd515ecc585ba
Keith E. Campbell <campbell@informatics.com>
2018-01-15 12:43:28 -0800

    Drag and drop support to taxonomy.

--- komet/gui-contracts/src/main/java/sh/komet/gui/control/ConceptLabel.java
+++ komet/gui-contracts/src/main/java/sh/komet/gui/control/ConceptLabel.java
@@ -55,7 +55,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
-import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DataFormat;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
