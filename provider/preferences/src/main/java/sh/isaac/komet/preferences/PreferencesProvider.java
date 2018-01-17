6b106a1c5704fe026cb4d82d5b0fb31940996f24
darmbrust <daniel.armbrust.list@gmail.com>
2018-01-17 11:39:15 -0600

    rewrite preferences provider so it stops blindly overwriting System properties, set up all the defaults it is imposting so they all operate relative to DATA_STORE_ROOT_LOCATION_PROPERTY, so that by setting that property, everything else will automatically move with it.

--- provider/preferences/src/main/java/sh/isaac/komet/preferences/PreferencesProvider.java
+++ provider/preferences/src/main/java/sh/isaac/komet/preferences/PreferencesProvider.java
@@ -44,23 +44,24 @@ package sh.isaac.komet.preferences;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import java.net.MalformedURLException;
 
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.util.function.Supplier;
 import java.util.prefs.Preferences;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-import static java.nio.file.StandardOpenOption.APPEND;
-import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+
+import org.apache.commons.lang3.StringUtils;
 
 //~--- non-JDK imports --------------------------------------------------------
 
@@ -115,78 +116,77 @@ public class PreferencesProvider
          } else {
             launchEnvironment = LaunchEnvironment.FX_LAUNCHER;
          }
-
-         switch (launchEnvironment) {
-         case FX_LAUNCHER: {
-            System.setProperty(DATA_STORE_ROOT_LOCATION_PROPERTY, "data/isaac.data");
-            System.setProperty(PREFERENCES_FOLDER_LOCATION, "data/preferences");
-
-            if (System.getProperty(IMPORT_FOLDER_LOCATION) == null) {
-               File importPath = new File("data/to-import");
-               importPath.mkdirs();
-               File afterImportPath = new File("data/completed-import");
-               afterImportPath.mkdirs();
-               System.setProperty(IMPORT_FOLDER_LOCATION, importPath.getAbsolutePath());
-               System.setProperty(AFTER_IMPORT_FOLDER_LOCATION, afterImportPath.getAbsolutePath());
-            } else {
-               File importPath = new File(System.getProperty(IMPORT_FOLDER_LOCATION));
-               importPath.mkdirs();
-            }
-
-            if (setPropertyIfFileExists(USER_CSS_LOCATION_PROPERTY, Paths.get("data", "user.css"))) {}
-            else if (setPropertyIfFileExists(USER_CSS_LOCATION_PROPERTY, Paths.get("user.css"))) {}
-            else {
-               Path cssPath = Paths.get("data", "user.css");
-
-               try (InputStream is = PreferencesProvider.class.getResourceAsStream("/user.css");
-                  BufferedWriter writer = new BufferedWriter(Files.newBufferedWriter(cssPath, CREATE, APPEND))) {
-                  BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-                  String         line   = null;
-
-                  while ((line = reader.readLine()) != null) {
-                     writer.write(line);
-                     writer.newLine();
+         
+         checkOrSetFileProperty("DATA_STORE_ROOT_LOCATION_PROPERTY", DATA_STORE_ROOT_LOCATION_PROPERTY, () -> {
+            return launchEnvironment == LaunchEnvironment.MAVEN ? 
+                  Paths.get("target", "data", "isaac.data").toFile() : 
+                     Paths.get("data", "isaac.data").toFile();
+         }, true);
+         
+         
+         checkOrSetFileProperty("PREFERENCES_FOLDER_LOCATION", PREFERENCES_FOLDER_LOCATION, () -> {
+            //Create it as a sibling folder of wherever the data store is specified
+            return Paths.get(System.getProperty(DATA_STORE_ROOT_LOCATION_PROPERTY)).resolveSibling("preferences").toFile();
+         }, true);
+         
+         checkOrSetFileProperty("IMPORT_FOLDER_LOCATION", IMPORT_FOLDER_LOCATION, () -> {
+            //Always look under a JVM relative path of data, rather than looking in the target folder.
+            return Paths.get("data", "to-import").toFile();
+         }, true);
+         
+         checkOrSetFileProperty("AFTER_IMPORT_FOLDER_LOCATION", AFTER_IMPORT_FOLDER_LOCATION, () -> {
+            //Create it as a sibling folder of wherever the data store is specified, this way, it gets
+            //removed with a maven clean, along with the rest of the DB.
+            return Paths.get(System.getProperty(DATA_STORE_ROOT_LOCATION_PROPERTY)).resolveSibling("completed-import").toFile();
+         }, true);
+         
+         checkOrSetFileProperty("USER_CSS_LOCATION_PROPERTY", USER_CSS_LOCATION_PROPERTY, () -> {
+            //db relative paths, followed by jvm relative paths, followed by writing out the default from the classpath.
+            try {
+               Path[] checkPaths = new Path[] {
+                     Paths.get(System.getProperty(DATA_STORE_ROOT_LOCATION_PROPERTY)).resolveSibling("data").resolve("user.css"),
+                     Paths.get("data", "user.css"), 
+                     Paths.get(System.getProperty(DATA_STORE_ROOT_LOCATION_PROPERTY)).resolveSibling("user.css"),
+                     Paths.get("user.css")};
+               
+               Path p = null;
+               for (int i = 0; i < checkPaths.length; i++) {
+                  if (Files.isRegularFile(checkPaths[i])) {
+                     p = checkPaths[i];
+                     break;
                   }
                }
-            }
+               if (p == null) {
+                  // Can't find one, write out the defaults as a sibling of the datastore
+                  // When running the build under maven, various tests wants to grab this file, from a project
+                  // that hasn't yet been built, so it isn't on the classpath.  Hack the actual path in place here.
+                  Path inputSource = Paths.get("komet", "css", "src", "main", "resources", "user.css");
 
-            break;
-         }
-
-         case MAVEN: {
-            System.setProperty(DATA_STORE_ROOT_LOCATION_PROPERTY, "target/data/isaac.data");
-            System.setProperty(PREFERENCES_FOLDER_LOCATION, "target/data/preferences");
-            if (System.getProperty(IMPORT_FOLDER_LOCATION) == null) {
-               File importPath = new File("data/to-import");
-               importPath.mkdirs();
-               File afterImportPath = new File("target/data/completed-import");
-               afterImportPath.mkdirs();
-               System.setProperty(IMPORT_FOLDER_LOCATION, importPath.getAbsolutePath());
-               System.setProperty(AFTER_IMPORT_FOLDER_LOCATION, afterImportPath.getAbsolutePath());
-            } else {
-               File importPath = new File(System.getProperty(IMPORT_FOLDER_LOCATION));
-               importPath.mkdirs();
-            }
-
-            if (setPropertyIfFileExists(
-                  USER_CSS_LOCATION_PROPERTY,
-                  Paths.get(
-                       "..",
-                      "css",
-                      "src",
-                      "main",
-                      "resources",
-                      "user.css"))) {}
-            else {
-               setPropertyIfFileExists(USER_CSS_LOCATION_PROPERTY, Paths.get("target", "data", "user.css"));
-            }
-
-            break;
-         }
-
-         default:
-            throw new UnsupportedOperationException("Can't handle launch environment: " + launchEnvironment);
-         }
+                  p = Paths.get(System.getProperty(DATA_STORE_ROOT_LOCATION_PROPERTY)).resolveSibling("user.css");
+                  try (InputStream is = (Files.isRegularFile(inputSource) ? 
+                        new FileInputStream(inputSource.toFile()) : 
+                           PreferencesProvider.class.getResourceAsStream("/user.css"));
+                        BufferedReader reader = (is == null ? null : new BufferedReader(new InputStreamReader(is)));
+                        BufferedWriter writer = (reader == null ? null : new BufferedWriter(Files.newBufferedWriter(p, CREATE_NEW)))) {
+                     
+                     if (is != null) {
+                        String line = null;
+   
+                        while ((line = reader.readLine()) != null) {
+                           writer.write(line);
+                           writer.newLine();
+                        }
+                     } else {
+                        LOG.info("Unable to locate a user.css file");
+                     }
+                  }
+               }
+               return p.toFile();
+            } catch (IOException e) {
+               LOG.error("Unexpected error trying to locate or write out the user.css file", e);
+               throw new RuntimeException(e);
+            } 
+         }, false);
 
          this.applicationPreferences = IsaacPreferencesImpl.getApplicationRoot();
 
@@ -216,12 +216,25 @@ public class PreferencesProvider
          LOG.info("Stopping Preferences Provider.");
          this.applicationPreferences.sync();
       } catch (Throwable ex) {
-         // HK2 swallows these exceptions, so I'm trying to make sure they are
-         // easy to identify.
          // TODO figure out how to keep hk2 from swallowing these.
-         ex.printStackTrace();
-         LOG.error(ex);
+         LOG.error("Unexpected error stopping prefs provider", ex);
          throw new RuntimeException(ex);
+      }
+   }
+   
+   private void checkOrSetFileProperty(String propertyName, String propertyKey, Supplier<File> defaultProvider, boolean makeDir) throws IOException {
+      File f;
+      if (StringUtils.isBlank(System.getProperty(propertyKey))) {
+         f = defaultProvider.get();
+         LOG.info("{}:{} is not set, using default of {}", propertyName, propertyKey, f.getCanonicalPath());
+         System.setProperty(propertyKey, f.getCanonicalPath());
+      }
+      else {
+         f = new File(System.getProperty(propertyKey)).getCanonicalFile();
+         LOG.info("{}:{} was already set to {}", propertyName, propertyKey, f.getCanonicalPath());
+      }
+      if (makeDir) {
+         f.mkdirs();
       }
    }
 
@@ -232,26 +245,6 @@ public class PreferencesProvider
       // MemoryConfiguration memoryConfiguration = MemoryConfiguration.ALL_CHRONICLES_IN_MEMORY;
       return IsaacPreferencesImpl.getApplicationRoot();
    }
-
-   //~--- set methods ---------------------------------------------------------
-
-   /**
-    *
-    * @return true if the file existed, and the property was set.
-    */
-   private boolean setPropertyIfFileExists(String property, Path filePath)
-            throws MalformedURLException {
-      if (Files.exists(filePath)) {
-         System.setProperty(property, filePath.toUri()
-               .toURL()
-               .toString());
-         return true;
-      }
-
-      return false;
-   }
-
-   //~--- get methods ---------------------------------------------------------
 
    @Override
    public IsaacPreferences getSystemPreferences() {
