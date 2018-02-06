2164929edbccff1e37d60ef7864e81fe56805292
darmbrust <daniel.armbrust.list@gmail.com>
2018-02-06 01:24:00 -0600

    open up the module coords that are used during a commit validation of a semantic rule so they work properly

--- core/api/src/main/java/sh/isaac/api/component/semantic/version/dynamic/DynamicValidatorType.java
+++ core/api/src/main/java/sh/isaac/api/component/semantic/version/dynamic/DynamicValidatorType.java
@@ -349,12 +349,14 @@ public enum DynamicValidatorType {
                
                return Get.taxonomyService().getStatedLatestSnapshot(
                      Get.stampService().getPathNidForStamp(stampSequence), 
-                     NidSet.of(Get.stampService().getModuleNidForStamp(stampSequence)),
+                     NidSet.EMPTY,  //the stamp sequence is only going to tell us the module this sememe is being created on, 
+                     //but often, the is_child_of check is about a different concept entirely, likely in a different module.
                      Status.ACTIVE_ONLY_SET).isChildOf(childId, parentId);
             } else {  //IS_KIND_OF
                return Get.taxonomyService().getStatedLatestSnapshot(
                       Get.stampService().getPathNidForStamp(stampSequence), 
-                      NidSet.of(Get.stampService().getModuleNidForStamp(stampSequence)),
+                      NidSet.EMPTY,  //the stamp sequence is only going to tell us the module this sememe is being created on, 
+                      //but often, the is_child_of check is about a different concept entirely, likely in a different module.
                       Status.ACTIVE_ONLY_SET).isKindOf(childId, parentId);
             }
          } catch (final IllegalArgumentException e) {
