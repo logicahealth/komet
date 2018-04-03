5833aff19ccd4126ad30088349b3534789618b29
darmbrust <daniel.armbrust.list@gmail.com>
2018-04-02 22:36:07 -0500

    just cleanup

--- provider/query/src/main/java/sh/isaac/provider/query/search/CompositeSearchResult.java
+++ provider/query/src/main/java/sh/isaac/provider/query/search/CompositeSearchResult.java
@@ -37,7 +37,6 @@
 
 package sh.isaac.provider.query.search;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map.Entry;
