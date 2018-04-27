c8c5c8fa804128c9cd69179bb1a8531f0b6f486d
darmbrust <daniel.armbrust.list@gmail.com>
2018-04-27 02:29:07 -0500

    continuing to expand the direct write helper framework

--- misc/importers/src/main/java/sh/isaac/convert/directUtils/DirectWriteHelper.java
+++ misc/importers/src/main/java/sh/isaac/convert/directUtils/DirectWriteHelper.java
@@ -25,11 +25,13 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
+import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import sh.isaac.MetaData;
 import sh.isaac.api.AssemblageService;
 import sh.isaac.api.Get;
+import sh.isaac.api.IdentifiedObjectService;
 import sh.isaac.api.IdentifierService;
 import sh.isaac.api.Status;
 import sh.isaac.api.TaxonomyService;
@@ -42,6 +44,15 @@ import sh.isaac.api.component.concept.ConceptService;
 import sh.isaac.api.component.semantic.SemanticBuilder;
 import sh.isaac.api.component.semantic.SemanticBuilderService;
 import sh.isaac.api.component.semantic.SemanticChronology;
+import sh.isaac.api.component.semantic.version.MutableDynamicVersion;
+import sh.isaac.api.component.semantic.version.StringVersion;
+import sh.isaac.api.component.semantic.version.dynamic.DynamicColumnInfo;
+import sh.isaac.api.component.semantic.version.dynamic.DynamicData;
+import sh.isaac.api.component.semantic.version.dynamic.DynamicDataType;
+import sh.isaac.api.component.semantic.version.dynamic.DynamicUsageDescription;
+import sh.isaac.api.component.semantic.version.dynamic.DynamicUtility;
+import sh.isaac.api.constants.DynamicConstants;
+import sh.isaac.api.externalizable.IsaacObjectType;
 import sh.isaac.api.index.IndexBuilderService;
 import sh.isaac.api.logic.LogicalExpression;
 import sh.isaac.api.logic.LogicalExpressionBuilder;
@@ -54,6 +65,8 @@ import sh.isaac.converters.sharedUtils.stats.ConverterUUID;
 import sh.isaac.converters.sharedUtils.stats.LoadStats;
 import sh.isaac.model.concept.ConceptChronologyImpl;
 import sh.isaac.model.semantic.SemanticChronologyImpl;
+import sh.isaac.model.semantic.types.DynamicStringImpl;
+import sh.isaac.model.semantic.types.DynamicUUIDImpl;
 import sh.isaac.model.semantic.version.ComponentNidVersionImpl;
 import sh.isaac.model.semantic.version.DescriptionVersionImpl;
 
@@ -74,6 +87,7 @@ public class DirectWriteHelper
 	private ConverterUUID converterUUID;
 	private ConceptService conceptService;
 	private IdentifierService identifierService;
+	private IdentifiedObjectService identifiedObjectService;
 	private StampService stampService;
 	private AssemblageService assemblageService;
 	private SemanticBuilderService<?> semanticBuilderService;
@@ -89,6 +103,8 @@ public class DirectWriteHelper
 	private Optional<UUID> refsetTypes = Optional.empty();
 	private Optional<UUID> relationTypes = Optional.empty();
 
+	private HashSet<UUID> associations = new HashSet<>();
+	
 	/**
 	 * If you create ANY logic graphs, ensure that you call {@link #processTaxonomyUpdates()} before allowing this helper to be 
 	 * garbage collected.
@@ -107,6 +123,7 @@ public class DirectWriteHelper
 		this.converterUUID = converterUUID;
 		this.conceptService = Get.conceptService();
 		this.identifierService = Get.identifierService();
+		this.identifiedObjectService = Get.identifiedObjectService();
 		this.stampService = Get.stampService();
 		this.assemblageService = Get.assemblageService();
 		this.semanticBuilderService = Get.semanticBuilderService();
@@ -151,6 +168,11 @@ public class DirectWriteHelper
 	}
 
 	/**
+	 * This creates a semantic type of {@link VersionType#DYNAMIC} with no data columns
+	 * 
+	 * This serves the same purpose as {@link #makeBrittleRefsetMember(UUID, UUID, long)} but allows for the refset to be described in the 
+	 * dynamic definition.
+	 * 
 	 * @param assemblageConcept The type of refset member to create
 	 * @param memberUUID The id being added to the refset
 	 * @param time The time to use for the entry
@@ -166,6 +188,62 @@ public class DirectWriteHelper
 		indexAndWrite(refsetMemberToWrite);
 		loadStats.addRefsetMember(Get.conceptDescriptionText(identifierService.getNidForUuids(assemblageConcept)));
 		return refsetMemberToWrite.getPrimordialUuid();
+	}
+	
+	/**
+	 * This creates a semantic type of {@link VersionType#MEMBER}
+	 * 
+	 * @param assemblageConcept The type of refset member to create
+	 * @param memberUUID The id being added to the refset
+	 * @param time The time to use for the entry
+	 * @return The UUID of the object created
+	 */
+	public UUID makeBrittleRefsetMember(UUID assemblageConcept, UUID memberUUID, long time)
+	{
+		UUID uuidForCreatedMember = UuidFactory.getUuidForDynamic(converterUUID.getNamespace(), assemblageConcept, memberUUID, null,
+				((input, uuid) -> converterUUID.addMapping(input, uuid)));
+		SemanticChronologyImpl refsetMemberToWrite = new SemanticChronologyImpl(VersionType.DYNAMIC,
+				uuidForCreatedMember, identifierService.getNidForUuids(assemblageConcept), identifierService.getNidForUuids(memberUUID));
+		refsetMemberToWrite.createMutableVersion(stampService.getStampSequence(Status.ACTIVE, time, authorNid, moduleNid, pathNid));
+		indexAndWrite(refsetMemberToWrite);
+		loadStats.addRefsetMember(Get.conceptDescriptionText(identifierService.getNidForUuids(assemblageConcept)));
+		return refsetMemberToWrite.getPrimordialUuid();
+	}
+	
+	/**
+	 * This creates a semantic type of {@link VersionType#DYNAMIC} with the specified data columns.
+	 * 
+	 * @param assemblageConcept The type of refset member to create
+	 * @param referencedComponent the referenced component this dynamic semantic entry is being added to
+	 * @param data optional - The data columns for this dynamic semantic entry
+	 * @param time The time to use for the entry
+	 * @return The UUID of the object created
+	 */
+	public UUID makeDynamicSemantic(UUID assemblageConcept, UUID referencedComponent, DynamicData[] data, long time)
+	{
+		UUID uuidForCreatedMember = UuidFactory.getUuidForDynamic(converterUUID.getNamespace(), assemblageConcept, referencedComponent, data,
+				((input, uuid) -> converterUUID.addMapping(input, uuid)));
+		SemanticChronologyImpl refsetMemberToWrite = new SemanticChronologyImpl(VersionType.DYNAMIC,
+				uuidForCreatedMember, identifierService.getNidForUuids(assemblageConcept), identifierService.getNidForUuids(referencedComponent));
+		MutableDynamicVersion<?> dv = refsetMemberToWrite.createMutableVersion(stampService.getStampSequence(Status.ACTIVE, time, authorNid, moduleNid, pathNid));
+		dv.setData(data);
+		indexAndWrite(refsetMemberToWrite);
+		loadStats.addAnnotation(identifiedObjectService.getChronology(identifierService.getNidForUuids(referencedComponent)).get().getIsaacObjectType().toString(),
+				converterUUID.getUUIDCreationString(assemblageConcept));
+		return refsetMemberToWrite.getPrimordialUuid();
+	}
+	
+	/**
+	 * create a dynamic semantic entry in the pattern that matches an assemblage dynamic semantic.
+	 * @param assemblageConcept the type of association to create
+	 * @param sourceConcept the source of the association
+	 * @param targetConcept the optional target of the association
+	 * @param time the time to make the changes at
+	 * @return the UUID of the object created
+	 */
+	public UUID makeAssociation(UUID assemblageConcept, UUID sourceConcept, UUID targetConcept, long time)
+	{
+		return makeDynamicSemantic(assemblageConcept, sourceConcept, targetConcept == null ? null : new DynamicData[] {new DynamicUUIDImpl(targetConcept)}, time);
 	}
 	
 	/**
@@ -186,6 +264,21 @@ public class DirectWriteHelper
 	{
 		return makeDescription(concept, text, descriptionType, MetaData.ENGLISH_LANGUAGE____SOLOR.getPrimordialUuid(), caseSignificance, status, time, 
 				MetaData.US_ENGLISH_DIALECT____SOLOR.getPrimordialUuid(), dialectAcceptibility);
+	}
+	
+	/**
+	 * Create a new dynamic semantic of type {@link DynamicConstants#DYNAMIC_COMMENT_ATTRIBUTE} in the corret format.
+	 * @param referencedComponent the item to place the comment on
+	 * @param comment the comment
+	 * @param commentContext the optional comment context
+	 * @param time the time to make the edit it
+	 * @return the created object
+	 */
+	public UUID makeComment(UUID referencedComponent, String comment, String commentContext, long time)
+	{
+		return makeDynamicSemantic(DynamicConstants.get().DYNAMIC_COMMENT_ATTRIBUTE.getPrimordialUuid(), referencedComponent, 
+				new DynamicData[] {new DynamicStringImpl(comment),
+						(StringUtils.isBlank(commentContext) ? null : new DynamicStringImpl(commentContext))}, time);
 	}
 	
 	/**
@@ -456,6 +549,81 @@ public class DirectWriteHelper
 			}
 		}
 	}
+	
+	/**
+	* Add all of the necessary metadata semantics onto the specified concept to make it a concept that defines a dynamic semantic assemblage
+	* See {@link DynamicUsageDescription} class for more details on this format.
+	* implemented by See {@link DynamicUtility#configureConceptAsDynamicSemantic(int, String, DynamicColumnInfo[], IsaacObjectType, VersionType, int)}
+	* 
+	* @param concept - The concept that will define a dynamic semantic
+	* @param dynamicUsageDescription - The description that describes the purpose of this dynamic semantic
+	* @param columns - optional - the columns of data that this dynamic semantic needs to be able to store.
+	* @param referencedComponentTypeRestriction - optional - any component type restriction info for the columns
+	* @param referencedComponentTypeSubRestriction - optional - any compont sub-type restrictions for the columns
+	* @param time - The time to use for this modification
+	*/
+	public void configureConceptAsDynamicAssemblage(UUID concept, String dynamicUsageDescription, DynamicColumnInfo[] columns,
+			IsaacObjectType referencedComponentTypeRestriction, VersionType referencedComponentTypeSubRestriction, long time)
+	{
+		int stampSequence = stampService.getStampSequence(Status.ACTIVE, time, authorNid, moduleNid, pathNid);
+		List<Chronology> items = Get.service(DynamicUtility.class).configureConceptAsDynamicSemantic(identifierService.getNidForUuids(concept), 
+				dynamicUsageDescription, columns, referencedComponentTypeRestriction, referencedComponentTypeSubRestriction, stampSequence);
+		
+		for (Chronology c : items)
+		{
+			indexAndWrite(c);
+		}
+	}
+	
+	/**
+	* Add all of the necessary metadata semantics onto the specified concept to make it an association.
+	* 
+	* This configures the concept as a dynamicSemantic, and then further adds association metadata.
+	* 
+	* @param concept - The concept that will define a dynamic semantic
+	* @param dynamicUsageDescription - The description that describes the purpose of this dynamic semantic
+	* @param inverseName - optional - the name to use when reading this association in reverse.  Do not create this name if it is identical to the forward name 
+	* @param associationComponentTypeRestriction optional restriction on which types of components can be associated
+	* @param associationComponentTypeSubRestriction  optional sub-restriction on which types of semantics can be associated.
+	* @param time - The time to use for this modification
+	*/
+	public void configureConceptAsAssociation(UUID concept, String dynamicUsageDescription, String inverseName, IsaacObjectType associationComponentTypeRestriction,
+			VersionType associationComponentTypeSubRestriction, long time)
+	{
+		//Make this a dynamic refex - with the association column info
+		DynamicColumnInfo[] columns = new DynamicColumnInfo[] {
+				new DynamicColumnInfo(0, DynamicConstants.get().DYNAMIC_COLUMN_ASSOCIATION_TARGET_COMPONENT.getPrimordialUuid(), DynamicDataType.UUID, null, false, true)};
+		configureConceptAsDynamicAssemblage(concept, dynamicUsageDescription, columns, associationComponentTypeRestriction, associationComponentTypeSubRestriction, time);
+
+		//Add this concept to the association refset
+		makeDynamicRefsetMember(DynamicConstants.get().DYNAMIC_ASSOCIATION.getPrimordialUuid(), concept, time);
+		
+		//Add inverse name, if provided
+		if (StringUtils.isNotBlank(inverseName))
+		{
+			//If inverse name equals forward (regular name) this is going to blow up...
+			UUID descriptionUUID = makeDescriptionEn(concept, inverseName, MetaData.REGULAR_NAME_DESCRIPTION_TYPE____SOLOR.getPrimordialUuid(), 
+					MetaData.DESCRIPTION_NOT_CASE_SENSITIVE____SOLOR.getPrimordialUuid(), Status.ACTIVE, time, MetaData.ACCEPTABLE____SOLOR.getPrimordialUuid());
+			makeDynamicRefsetMember(DynamicConstants.get().DYNAMIC_ASSOCIATION_INVERSE_NAME.getPrimordialUuid(), descriptionUUID, time);
+		}
+		associations.add(concept);
+	}
+	
+	/**
+	 * The current usage of the {@link MetaData#IDENTIFIER_ASSEMBALGE____SOLOR} refset is for {@link StringVersion} semantics 
+	 * which happen to contain a value that is an identifier.
+	 * 
+	 * Assemblage concepts marked with this identifier assemblage get special treatment in search patterns.
+	 * 
+	 * You should only call this method if the passed in concept is being used as the assemblage concept for {@link StringVersion} semantics. 
+	 * 
+	 * @param concept The concept defining a {@link StringVersion} semantic assemblage, which is also carrying identifier data.
+	 * @param time the time to use for making the modifications.
+	 */
+	public void configureConceptAsIdentifier(UUID concept, long time)
+	{
+		makeBrittleRefsetMember(MetaData.IDENTIFIER_ASSEMBALGE____SOLOR.getPrimordialUuid(), concept, time);
+	}
 
 	/**
 	 * @return The UUID of the association types node from {@link #makeMetadataHierarchy(String, boolean, boolean, boolean, boolean, boolean, long)}, 
@@ -500,5 +668,15 @@ public class DirectWriteHelper
 	public Optional<UUID> getRelationTypes()
 	{
 		return relationTypes;
+	}
+	
+	/**
+	 * @return true, if the concept was configured {@link #configureConceptAsAssociation(UUID, String, String, IsaacObjectType, VersionType, long)} 
+	 * as an association
+	 * @param concept the concept to check
+	 */
+	public boolean isAssociation(UUID concept)
+	{
+		return associations.contains(concept);
 	}
 }
