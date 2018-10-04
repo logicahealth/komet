d3eb83c9af9fdf0764267ccea01d8a2330af3c69
darmbrust <daniel.armbrust.list@gmail.com>
2018-10-04 16:57:57 -0500

    performance improvement by removing unsed code

--- komet/taxonomy-view/src/main/java/sh/isaac/komet/gui/treeview/DefaultMultiParentTreeItemDisplayPolicies.java
+++ komet/taxonomy-view/src/main/java/sh/isaac/komet/gui/treeview/DefaultMultiParentTreeItemDisplayPolicies.java
@@ -73,7 +73,6 @@ public class DefaultMultiParentTreeItemDisplayPolicies implements MultiParentTre
    @Override
    public boolean shouldDisplay(MultiParentTreeItem treeItem) {
       int conceptNid = treeItem.getConceptNid();
-      String description = Get.conceptDescriptionText(conceptNid);
       EnumSet<Status> allowedStates = manifold.getAllowedStates();
       EnumSet<Status> states = Get.conceptActiveService().getConceptStates(conceptNid, manifold);
       for (Status state: states) {
