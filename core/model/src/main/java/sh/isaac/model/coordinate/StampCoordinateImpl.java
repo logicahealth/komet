mindcomputing/knowledge-core

add another makeAnalog pattern to the stamp coordinate for modules

core/model/src/main/java/sh/isaac/model/coordinate/StampCoordinateImpl.java
@@ -273,8 +273,21 @@ public class StampCoordinateImpl
    public StampCoordinate makeCoordinateAnalog(EnumSet<Status> states) {
       return new StampCoordinateImpl(this.stampPrecedence, this.stampPosition, this.moduleNids, this.modulePriorityList, states);
    }
-
+   
    /**
+    * @see sh.isaac.api.coordinate.StampCoordinate#makeModuleAnalog(int[], boolean)
+    */
+   @Override
+   public StampCoordinate makeModuleAnalog(int[] modules, boolean add) {
+      NidSet newNids = new NidSet();
+      newNids.addAll(modules);
+      if (add) {
+         newNids.addAll(this.moduleNids);
+      }
+      return new StampCoordinateImpl(this.stampPrecedence, this.stampPosition, newNids, this.modulePriorityList, this.allowedStates);
+   }
+
+/**
     * To string.
     *
     * @return the string

logicahealth/komet

Conflicts:
	core/api/src/main/java/sh/isaac/api/coordinate/StampCoordinate.java

core/model/src/main/java/sh/isaac/model/coordinate/StampCoordinateImpl.java
@@ -270,8 +270,21 @@ public class StampCoordinateImpl
    public StampCoordinate makeCoordinateAnalog(EnumSet<Status> states) {
       return new StampCoordinateImpl(this.stampPrecedence, this.stampPosition, this.moduleSpecifications, this.modulePriorityList, states);
    }
-
+   
    /**
+    * @see sh.isaac.api.coordinate.StampCoordinate#makeModuleAnalog(Collection<ConceptSpecification>, boolean)
+    */
+   @Override
+   public StampCoordinate makeModuleAnalog(Collection<ConceptSpecification> modules, boolean add) {
+      HashSet<ConceptSpecification> newNids = new HashSet<>();
+      newNids.addAll(modules);
+      if (add) {
+         newNids.addAll(this.moduleSpecifications);
+      }
+      return new StampCoordinateImpl(this.stampPrecedence, this.stampPosition, newNids, this.modulePriorityList, this.allowedStates);
+   }
+
+/**
     * To string.
     *
     * @return the string