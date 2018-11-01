mindcomputing/knowledge-core

add an API for answering taxonomy questions without building the entire tree.

core/api/src/main/java/sh/isaac/api/TaxonomyService.java
@@ -55,6 +55,7 @@ import sh.isaac.api.collections.NidSet;
 import sh.isaac.api.coordinate.ManifoldCoordinate;
 import sh.isaac.api.coordinate.PremiseType;
 import sh.isaac.api.component.semantic.SemanticChronology;
+import sh.isaac.api.tree.Tree;
 import sh.isaac.api.tree.TreeNodeVisitData;
 
 //~--- interfaces -------------------------------------------------------------
@@ -112,12 +113,30 @@ public interface TaxonomyService
    IntStream getAllRelationshipOriginNidsOfType(int destinationConceptNid, IntSet typeConceptNidSet);
 
    /**
-    * Gets the snapshot.
+    * Gets the snapshot.  This method is for returning a Snapshot that builds an entire tree in a background thread.
+    * The returned {@link TaxonomySnapshotService} can be used immediately, while it computes in the background - until
+    * the entire tree is computed, it will answer queries via direct lookups.  After the tree is computed, it will use
+    * the case to answer queries.  This approach is best for a use case where the TaxonomySnapshotService will be used 
+    * for many queries for a period of time.
     *
     * @param tc the tc
-    * @return the snapshot
+    * @return the snapshot which is backed by a {@link Tree}, although that tree may not be complete for some time after
+    * this call returns.
     */
    TaxonomySnapshotService getSnapshot(ManifoldCoordinate tc);
+   
+   /**
+    * Gets the snapshot.  This method is for returning a Snapshot that does NOT build a tree in the background.
+    * Every query will be answered by direct computation on the call.  Implementations may do some caching of answers
+    * previously computed.  
+    * 
+    * This approach is best for a use case where the TaxonomySnapshotService will be used to answer a single query, and 
+    * then be thrown away.
+    *
+    * @param mc the manifold coordinate
+    * @return the snapshot that is NOT backed by a {@link Tree}
+    */
+   TaxonomySnapshotService getSnapshotNoTree(ManifoldCoordinate mc);
    
 
 	/**
@@ -127,9 +146,11 @@ public interface TaxonomyService
 	 * @param pathNid
 	 * @param modules
 	 * @param allowedStates
+	 * @param computeTree true, if this should call {@link #getSnapshot(ManifoldCoordinate)}, false if it should call
+	 * {@link #getSnapshotNoTree(ManifoldCoordinate)}
 	 * @return the Snapshot service
 	 */
-	TaxonomySnapshotService getStatedLatestSnapshot(int pathNid, NidSet modules, EnumSet<Status> allowedStates);
+	TaxonomySnapshotService getStatedLatestSnapshot(int pathNid, NidSet modules, EnumSet<Status> allowedStates, boolean computeTree);
    
    /**
     * 

misc/util/src/main/java/sh/isaac/utility/Frills.java
@@ -489,7 +489,7 @@ public class Frills
          final LanguageCoordinate fqnCoord = LanguageCoordinates.getUsEnglishLanguageFullySpecifiedNameCoordinate();
          
          //iterate the children, find one that has a FQN than ends with "Edit (SOLOR)"
-         int[] termTypeChildren = Get.taxonomyService().getSnapshot(new ManifoldCoordinateImpl(stamp, fqnCoord)).getTaxonomyChildConceptNids(termTypeConcept);
+         int[] termTypeChildren = Get.taxonomyService().getSnapshotNoTree(new ManifoldCoordinateImpl(stamp, fqnCoord)).getTaxonomyChildConceptNids(termTypeConcept);
          for (int nid : termTypeChildren) {
             String fqn = fqnCoord.getFullyQualifiedName(nid, stamp).orElseGet(() -> "");
             int index = fqn.indexOf("Edit (" + ConceptProxy.METADATA_SEMANTIC_TAG + ")"); 
@@ -543,7 +543,7 @@ public class Frills
          }
       }
       
-      int[] parents = Get.taxonomyService().getSnapshot(
+      int[] parents = Get.taxonomyService().getSnapshotNoTree(
             new ManifoldCoordinateImpl(stampToUse, LanguageCoordinates.getUsEnglishLanguageFullySpecifiedNameCoordinate()))
             .getTaxonomyParentConceptNids(conceptModuleNid);
       for (int current : parents)
@@ -1106,7 +1106,7 @@ public class Frills
     */
    public static Set<Integer> getAllChildrenOfConcept(int conceptNid, boolean recursive, boolean leafOnly, StampCoordinate stamp) {
       
-      TaxonomySnapshotService tss = Get.taxonomyService().getSnapshot(
+      TaxonomySnapshotService tss = Get.taxonomyService().getSnapshotNoTree(
             new ManifoldCoordinateImpl((stamp == null ? Get.configurationService().getUserConfiguration(Optional.empty()).getStampCoordinate() : stamp),
                   LanguageCoordinates.getUsEnglishLanguageFullySpecifiedNameCoordinate()));
       
@@ -2141,7 +2141,7 @@ public class Frills
       TaxonomySnapshotService tss = Get.taxonomyService().getStatedLatestSnapshot(
             (stamp == null ? StampCoordinates.getDevelopmentLatest().getStampPosition().getStampPathNid() : stamp.getStampPosition().getStampPathNid()),
             (stamp == null ? NidSet.EMPTY : stamp.getModuleNids()),
-            (stamp == null ? Status.ACTIVE_ONLY_SET : stamp.getAllowedStates()));
+            (stamp == null ? Status.ACTIVE_ONLY_SET : stamp.getAllowedStates()), false);
 
       if (stamp == null) {
          for (int stampSequence : oc.getVersionStampSequences()) {

provider/query/src/main/java/sh/isaac/provider/query/lucene/indexers/DescriptionIndexer.java
@@ -154,7 +154,7 @@ public class DescriptionIndexer extends LuceneIndexer
             try 
             {
                //cache doesn't have the answer, needs to calculate.  We construct a snapshot of latest time, the path, and any module, active only.
-               TaxonomySnapshotService tss = Get.taxonomyService().getSnapshot(new ManifoldCoordinateImpl(
+               TaxonomySnapshotService tss = Get.taxonomyService().getSnapshotNoTree(new ManifoldCoordinateImpl(
                      new StampCoordinateImpl(StampPrecedence.PATH, new StampPositionImpl(Long.MAX_VALUE, pathNid), NidSet.EMPTY, new int[0], Status.ACTIVE_ONLY_SET), null));
                return tss.isKindOf(semanticChronology.getReferencedComponentNid(), TermAux.SOLOR_METADATA.getNid());
             }

logicahealth/komet

Conflicts:
	core/api/src/main/java/sh/isaac/api/TaxonomyService.java
	core/api/src/main/java/sh/isaac/api/TaxonomySnapshotService.java
	misc/differ/src/main/java/sh/isaac/convert/differ/IBDFDiff.java
	misc/util/src/main/java/sh/isaac/utility/Frills.java
	provider/chronology/src/main/java/sh/isaac/provider/datastore/taxonomy/TaxonomyProvider.java
	provider/query/src/main/java/sh/isaac/provider/query/lucene/indexers/DescriptionIndexer.java

core/api/src/main/java/sh/isaac/api/TaxonomyService.java
@@ -57,6 +57,7 @@ import sh.isaac.api.component.concept.ConceptSpecification;
 import sh.isaac.api.coordinate.ManifoldCoordinate;
 import sh.isaac.api.coordinate.PremiseType;
 import sh.isaac.api.component.semantic.SemanticChronology;
+import sh.isaac.api.tree.Tree;
 import sh.isaac.api.tree.TreeNodeVisitData;
 
 //~--- interfaces -------------------------------------------------------------
@@ -114,24 +115,43 @@ public interface TaxonomyService
    IntStream getAllRelationshipOriginNidsOfType(int destinationConceptNid, IntSet typeConceptNidSet);
 
    /**
-    * Gets the snapshot.
+    * Gets the snapshot.  This method is for returning a Snapshot that builds an entire tree in a background thread.
+    * The returned {@link TaxonomySnapshotService} can be used immediately, while it computes in the background - until
+    * the entire tree is computed, it will answer queries via direct lookups.  After the tree is computed, it will use
+    * the case to answer queries.  This approach is best for a use case where the TaxonomySnapshotService will be used 
+    * for many queries for a period of time.
     *
     * @param tc the tc
-    * @return the snapshot
+    * @return the snapshot which is backed by a {@link Tree}, although that tree may not be complete for some time after
+    * this call returns.
     */
    TaxonomySnapshot getSnapshot(ManifoldCoordinate tc);
    
+   /**
+    * Gets the snapshot.  This method is for returning a Snapshot that does NOT build a tree in the background.
+    * Every query will be answered by direct computation on the call.  Implementations may do some caching of answers
+    * previously computed.  
+    * 
+    * This approach is best for a use case where the TaxonomySnapshotService will be used to answer a single query, and 
+    * then be thrown away.
+    *
+    * @param mc the manifold coordinate
+    * @return the snapshot that is NOT backed by a {@link Tree}
+    */
+   TaxonomySnapshot getSnapshotNoTree(ManifoldCoordinate mc);
 
-	/**
-	 * Calls {@link #getSnapshot(ManifoldCoordinate)} with a manifold constructed from the provided path, 
-	 * modules, and states.  Uses {@link PremiseType#STATED} and a time of MAX_VALUE.  Language is set to the 
-	 * system default.
-	 * @param pathNid
-	 * @param modules
-	 * @param allowedStates
-	 * @return the Snapshot service
-	 */
-	TaxonomySnapshot getStatedLatestSnapshot(int pathNid, Set<ConceptSpecification> modules, EnumSet<Status> allowedStates);
+    /**
+     * Calls {@link #getSnapshot(ManifoldCoordinate)} with a manifold constructed from the provided path, 
+     * modules, and states.  Uses {@link PremiseType#STATED} and a time of MAX_VALUE.  Language is set to the 
+     * system default.
+     * @param pathNid
+     * @param modules
+     * @param allowedStates
+     * @param computeTree true, if this should call {@link #getSnapshot(ManifoldCoordinate)}, false if it should call
+     * {@link #getSnapshotNoTree(ManifoldCoordinate)}
+     * @return the Snapshot service
+     */
+   TaxonomySnapshot getStatedLatestSnapshot(int pathNid, Set<ConceptSpecification> modules, EnumSet<Status> allowedStates, boolean computeTree);
    
    /**
     * 

misc/util/src/main/java/sh/isaac/utility/Frills.java
@@ -489,7 +489,7 @@ public class Frills
          final LanguageCoordinate fqnCoord = LanguageCoordinates.getUsEnglishLanguageFullySpecifiedNameCoordinate();
          
          //iterate the children, find one that has a FQN than ends with "Edit (SOLOR)"
-         int[] termTypeChildren = Get.taxonomyService().getSnapshot(new ManifoldCoordinateImpl(stamp, fqnCoord)).getTaxonomyChildConceptNids(termTypeConcept);
+         int[] termTypeChildren = Get.taxonomyService().getSnapshotNoTree(new ManifoldCoordinateImpl(stamp, fqnCoord)).getTaxonomyChildConceptNids(termTypeConcept);
          for (int nid : termTypeChildren) {
             String fqn = fqnCoord.getFullyQualifiedName(nid, stamp).orElseGet(() -> "");
             int index = fqn.indexOf("Edit (" + ConceptProxy.METADATA_SEMANTIC_TAG + ")"); 
@@ -543,7 +543,7 @@ public class Frills
          }
       }
       
-      int[] parents = Get.taxonomyService().getSnapshot(
+      int[] parents = Get.taxonomyService().getSnapshotNoTree(
             new ManifoldCoordinateImpl(stampToUse, LanguageCoordinates.getUsEnglishLanguageFullySpecifiedNameCoordinate()))
             .getTaxonomyParentConceptNids(conceptModuleNid);
       for (int current : parents)
@@ -1106,7 +1106,7 @@ public class Frills
     */
    public static Set<Integer> getAllChildrenOfConcept(int conceptNid, boolean recursive, boolean leafOnly, StampCoordinate stamp) {
       
-      TaxonomySnapshot tss = Get.taxonomyService().getSnapshot(
+      TaxonomySnapshot tss = Get.taxonomyService().getSnapshotNoTree(
             new ManifoldCoordinateImpl((stamp == null ? Get.configurationService().getUserConfiguration(Optional.empty()).getStampCoordinate() : stamp),
                   LanguageCoordinates.getUsEnglishLanguageFullySpecifiedNameCoordinate()));
       
@@ -2139,9 +2139,9 @@ public class Frills
       HashSet<Integer> terminologyTypes = new HashSet<>();
       
       TaxonomySnapshot tss = Get.taxonomyService().getStatedLatestSnapshot(
-            (stamp == null ? StampCoordinates.getDevelopmentLatest().getStampPosition().getStampPathSpecification().getNid() : stamp.getStampPosition().getStampPathSpecification().getNid()),
-            (stamp == null ? new HashSet<>() : stamp.getModuleSpecifications()),
-            (stamp == null ? Status.ACTIVE_ONLY_SET : stamp.getAllowedStates()));
+              (stamp == null ? StampCoordinates.getDevelopmentLatest().getStampPosition().getStampPathSpecification().getNid() : stamp.getStampPosition().getStampPathSpecification().getNid()),
+              (stamp == null ? new HashSet<>() : stamp.getModuleSpecifications()),
+              (stamp == null ? Status.ACTIVE_ONLY_SET : stamp.getAllowedStates()), false);
 
       if (stamp == null) {
          for (int stampSequence : oc.getVersionStampSequences()) {

provider/query/src/main/java/sh/isaac/provider/query/lucene/indexers/DescriptionIndexer.java
@@ -154,7 +154,7 @@ public class DescriptionIndexer extends LuceneIndexer
             try 
             {
                //cache doesn't have the answer, needs to calculate.  We construct a snapshot of latest time, the path, and any module, active only.
-               TaxonomySnapshot tss = Get.taxonomyService().getSnapshot(new ManifoldCoordinateImpl(
+               TaxonomySnapshot tss = Get.taxonomyService().getSnapshotNoTree(new ManifoldCoordinateImpl(
                      new StampCoordinateImpl(StampPrecedence.PATH, new StampPositionImpl(Long.MAX_VALUE, pathNid), new HashSet<>(), new ArrayList<>(), Status.ACTIVE_ONLY_SET), null));
                return tss.isKindOf(semanticChronology.getReferencedComponentNid(), TermAux.SOLOR_METADATA.getNid());
             }