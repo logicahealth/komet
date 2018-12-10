97bc2f9e62d565568d0af63daf7a647472427406
darmbrust <daniel.armbrust.list@gmail.com>
2018-12-10 11:49:42 -0600

    loaders no longer require metadata to be preloaded, and fix a bug from mavens inconsistent handling of multi-properties.
    Conflicts:
    	integration/db-config-builder/src/main/java/sh/isaac/pombuilder/converter/ContentConverterCreator.java

--- integration/db-config-builder/src/main/java/sh/isaac/pombuilder/converter/ContentConverterCreator.java
+++ integration/db-config-builder/src/main/java/sh/isaac/pombuilder/converter/ContentConverterCreator.java
@@ -182,14 +182,6 @@ public class ContentConverterCreator
 					unpackArtifacts.append(ibdf.getArtifactId());
 					unpackArtifacts.append(",");
 				}
-
-				temp = FileUtil.readFile("converterProjectTemplate/pomSnippits/ibdfDependency.xml");
-				temp = temp.replace("#GROUPID#", "sh.isaac.core");
-				temp = temp.replace("#ARTIFACTID#", "metadata");
-				temp = temp.replace("#CLASSIFIER#", "all");
-				temp = temp.replace("#VERSION#", converterVersion);
-				dependencies.append(temp);
-				unpackArtifacts.append("metadata");
 				unpackDependencies = unpackDependencies.replace("#UNPACK_ARTIFACTS#", unpackArtifacts.toString());
 			}
 
@@ -249,7 +241,7 @@ public class ContentConverterCreator
 						{
 							if (option.getKey().isAllowMultiSelectInPomMode())
 							{
-								userOptions.append(optionIndent + "<" + option.getKey().getInternalName() + "s>\n");
+								userOptions.append(optionIndent + "<" + option.getKey().getInternalName() + ">\n");
 
 								for (final String value : option.getValue())
 								{
@@ -270,7 +262,7 @@ public class ContentConverterCreator
 									userOptions.append("</" + option.getKey().getInternalName() + ">\n");
 								}
 
-								userOptions.append(optionIndent + "</" + option.getKey().getInternalName() + "s>");
+								userOptions.append(optionIndent + "</" + option.getKey().getInternalName() + ">");
 							}
 							else
 							{
