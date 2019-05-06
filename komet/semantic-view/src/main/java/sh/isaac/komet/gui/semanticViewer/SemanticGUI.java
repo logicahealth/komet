f5fb61bc8a08acd5e35a59e082ba37505b6ec175
darmbrust <daniel.armbrust.list@gmail.com>
2019-05-06 17:33:55 -0500

    use the provided language coord

--- komet/semantic-view/src/main/java/sh/isaac/komet/gui/semanticViewer/SemanticGUI.java
+++ komet/semantic-view/src/main/java/sh/isaac/komet/gui/semanticViewer/SemanticGUI.java
@@ -434,7 +434,7 @@ public class SemanticGUI
 			}
 			else if (oc.get() instanceof ConceptChronology)
 			{
-				Optional<String> conDesc = Frills.getDescription(oc.get().getNid(), manifold_.getStampCoordinate(), null);
+				Optional<String> conDesc = Frills.getDescription(oc.get().getNid(), manifold_.getStampCoordinate(), manifold_.getLanguageCoordinate());
 				text = (conDesc.isPresent() ? conDesc.get() : "off path [NID]:" + oc.get().getNid());
 			}
 			else if (oc.get() instanceof SemanticChronology)
