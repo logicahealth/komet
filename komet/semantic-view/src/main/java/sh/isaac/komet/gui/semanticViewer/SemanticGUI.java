4308a4ba8ba6aa1df1917fabe9b4efaa075b0ade
darmbrust <daniel.armbrust.list@gmail.com>
2019-05-22 15:34:14 -0500

    fix a bug with the display of description types

--- komet/semantic-view/src/main/java/sh/isaac/komet/gui/semanticViewer/SemanticGUI.java
+++ komet/semantic-view/src/main/java/sh/isaac/komet/gui/semanticViewer/SemanticGUI.java
@@ -632,8 +632,8 @@ public class SemanticGUI
 				return new DynamicData[] {new DynamicNidImpl(((ComponentNidVersion)semantic).getComponentNid())};
 			case DESCRIPTION:
 				return new DynamicData[] {new DynamicStringImpl(((DescriptionVersion)semantic).getText()), 
-						new DynamicNidImpl(((DescriptionVersion)semantic).getDescriptionTypeConceptNid()),
 						new DynamicNidImpl(((DescriptionVersion)semantic).getLanguageConceptNid()),
+						new DynamicNidImpl(((DescriptionVersion)semantic).getDescriptionTypeConceptNid()),
 						new DynamicNidImpl(((DescriptionVersion)semantic).getCaseSignificanceConceptNid())};
 			case DYNAMIC:
 				return ((DynamicVersion<?>)semantic).getData();
