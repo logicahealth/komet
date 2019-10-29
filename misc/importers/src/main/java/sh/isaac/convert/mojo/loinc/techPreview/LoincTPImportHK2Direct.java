8aa8889fe0b25d73fbc0c059a76643e054751ca9
darmbrust <daniel.armbrust.list@gmail.com>
2019-10-29 16:05:04 -0500

    revert changes here - was editing the wrong class.

--- misc/importers/src/main/java/sh/isaac/convert/mojo/loinc/techPreview/LoincTPImportHK2Direct.java
+++ misc/importers/src/main/java/sh/isaac/convert/mojo/loinc/techPreview/LoincTPImportHK2Direct.java
@@ -161,16 +161,8 @@ public class LoincTPImportHK2Direct extends DirectConverterBaseMojo implements D
 	@Override
 	protected Collection<VersionType> getIBDFSkipTypes()
 	{
-		//The tech preview conversion needs descriptions.  We could probably skip some other stuff, but it wouldn't help performance that much...
-		//acceptabilities
-		return  Arrays.asList(new VersionType[] {VersionType.COMPONENT_NID});
-	}
-	
-	@Override
-	protected boolean IBDFPreloadActiveOnly()
-	{
-		//The tech preview conversion needs some inactive concepts
-		return false;
+		//skip descriptions, acceptabilities
+		return  Arrays.asList(new VersionType[] {VersionType.DESCRIPTION, VersionType.COMPONENT_NID});
 	}
 
 	/**
